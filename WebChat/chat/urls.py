78494e9a145b1fcd8243ea8d9b9ca79895cf27eb
Moises <lawliex@gmail.com>
2020-06-25 00:07:36 +1200

    .

---
+++ WebChat/chat/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import IndexView, RoomView
+
+app_name = 'chat'
+urlpatterns = [
+	path('',IndexView.as_view(), name = 'index'),
+	path('chat/<str:room_name>/', views.RoomView.as_view(), name='room')
+]
